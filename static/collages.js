9d83cf79a2ac5e39e165127d846a21654ba3b805
Valentin Iovene <valentin@too.gy>
2021-01-18 09:49:12 +0100

    deploy: Mon Jan 18 09:49:12 CET 2021

--- static/collages.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".collage").hover(function() {
-        $(".collage").css("opacity", "0.8");
-        $(this).css("opacity", "1.0");
-    }, function() {
-        $(".collage").css("opacity", "1.0");
-    });
-});
