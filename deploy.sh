64ab6bdcec9fc3411a81ae0189a3a90284f4c79b
Valentin Iovene <valentin@too.gy>
2021-01-18 09:48:37 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+# fail if any command fails
+set -e
+# make sure we're on the develop branch
+git checkout develop
+# build the website there (outputed in the unversioned _build directory)
+make
+# copy _build to /tmp
+if [ -d /tmp/_build ]; then
+    rm -rf /tmp/_build
+fi
+cp -r _build /tmp/_build
+# checkout main branch, containing the static website
+git checkout main
+# remove all files (note: this will not remove hidden files such as the .git
+# folder, which should not be removed of course)
+rm -rf *
+# copy build content here
+cp -r /tmp/_build/* .
+# push that to the repo
+git add .
+DATE=`date`
+git commit -m "deploy: ${DATE}"
+git push github main
